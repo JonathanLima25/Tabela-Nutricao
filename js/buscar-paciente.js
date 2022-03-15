ad168f61829ee7eb5b2456563428885e1467fc23
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-03-15 20:53:52 -0300

    adiciona mensagem de erro

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -1,15 +1,22 @@
 let botaoAdicionar = document.querySelector('#buscar-paciente');
 
+
 botaoAdicionar.addEventListener("click", function(){
     let xhr = new XMLHttpRequest();
 
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
     xhr.addEventListener("load", function(){
-        let resposta = xhr.responseText;
-        let pacientes = JSON.parse(resposta);
-        pacientes.forEach(paciente => {
-            adicionaPaciente(paciente)
-        });
+        if(xhr.status == 200){
+            let resposta = xhr.responseText;
+            let pacientes = JSON.parse(resposta);
+            pacientes.forEach(paciente => {
+                adicionaPaciente(paciente)
+            });
+        }else{
+            let mensagemErro = document.querySelector('#erro-busca');
+            mensagemErro.classList.remove('invisivel');
+            
+        }
     })
     xhr.send();
 })
