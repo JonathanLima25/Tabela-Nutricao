817d1071004ff353f892cfba89e0ef02bbde3524
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-03-15 18:33:03 -0300

    Verifica se existe campos em branco

--- js/form.js
+++ js/form.js
@@ -75,6 +75,14 @@ function obtemPacienteFormulario(form) {
 function validaPaciente(paciente){
     let erros = [];
 
+    if(paciente.nome.length == 0) erros.push("Nome não pode ser em branco");
+
+    if(paciente.peso.length == 0) erros.push("Peso não pode ser em branco");
+
+    if(paciente.altura.length == 0) erros.push("Altura não pode ser em branco");
+
+    if(paciente.gordura.length == 0) erros.push("Gordura não pode ser em branco");
+    
     if(!validaPeso(paciente.peso)) erros.push("O peso é inválido!");
 
     if(!validaAltura(paciente.altura)) erros.push("Altura inválida!");
