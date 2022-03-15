df153f5fe4258f931757b2bd47fc3cdd2af268e0
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-03-15 19:12:55 -0300

    removendo pacientes adicionados

--- js/remover.js
+++ js/remover.js
@@ -1,5 +1,12 @@
 let pacientes = document.querySelectorAll('.paciente');
 
+let tabela = document.querySelector('tbody');
+
+tabela.addEventListener("dblclick", function(event) {
+    let alvo = event.target;
+    alvo.parentNode.remove();
+})
+
 pacientes.forEach(function(paciente){
     paciente.addEventListener("dblclick", function(){
         paciente.remove()
