ad4647b131c05e53739614701780fa0c8c3d5dcc
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-03-15 20:15:45 -0300

    Filtra com regex

--- js/filtra.js
+++ js/filtra.js
@@ -1,15 +1,22 @@
 let campoFiltro = document.querySelector('#filtrar-tabela');
 
 campoFiltro.addEventListener('input', function(){
-    console.log(this.value)
+    
     let pacientes = document.querySelectorAll('.paciente');
-    pacientes.forEach(function(paciente){
-        let nome = paciente.querySelector('.info-nome').textContent;
-        
-        if(nome != campoFiltro.value){
-            paciente.classList.add('invisivel');
-        } else {
+    if(this.value.length > 0 ){
+        pacientes.forEach(function(paciente){
+            let nome = paciente.querySelector('.info-nome').textContent;
+            let expressao = new RegExp(campoFiltro.value, "i")
+            
+            if(!expressao.test(nome)){
+                paciente.classList.add('invisivel');
+            } else {
+                paciente.classList.remove('invisivel');
+            }
+        })
+    }else{
+        pacientes.forEach(function(paciente){
             paciente.classList.remove('invisivel');
-        }
-    })
+        })
+    }
 })
